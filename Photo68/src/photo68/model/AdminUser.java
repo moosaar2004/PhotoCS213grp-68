eee8709a0bf146d6e6c06798eb10d10c07ce340a
moosaar2004 <moosaar2001@gmail.com>
2024-04-06 00:40:40 -0400

    UserUpdates

--- Photo68/src/photo68/model/AdminUser.java
+++ Photo68/src/photo68/model/AdminUser.java
@@ -1,5 +1,173 @@
 package photo68.model;
 
-public class AdminUser{
-    
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * @author Ali Rehman
+ */
+public class AdminUser implements Serializable
+{
+    public static final String storeDirectory = "data";
+    public static final String fileName = "user.ser";
+
+    public ArrayList<User> allUsers;
+    public User currentUser;
+    public boolean userLoggedIn;
+
+    public AdminUser()
+    {
+        allUsers = new ArrayList<>();
+        allUsers.add(new User("admin"));
+        this.currentUser = null;
+        this.userLoggedIn = false;
+    }
+
+    /**
+     * set the list of users
+     * @param users the list of users
+     */
+    public void setUsers(ArrayList<User> users)
+    {
+        this.allUsers = users;
+    }
+
+    /**
+     * getter for current user
+     * @return current user
+     */
+    public User getCurrentUser()
+    {
+        return currentUser;
+    }
+
+    /**
+     * setter for current user
+     * @param user to be set as current
+     */
+    public void setCurrentUser(User user)
+    {
+        this.currentUser = user;
+    }
+
+    /**
+     * Getter for list of all users
+     * @return list of all users
+     */
+    public ArrayList<User> getAllUsers()
+    {
+        return allUsers;
+    }
+
+    /**
+     * Add user to list
+     * @param username of the new user to add
+     */
+    public void addUserToList(String username)
+    {
+        allUsers.add(new User(username));
+    }
+
+    /**
+     * delete user from list
+     * @param username of the new user to delete
+     */
+    public void deleteUserByNameFromList(String username)
+    {
+        allUsers.remove(username);
+    }
+
+    /**
+     * delete user by index in list
+     * @param index of the user to delete in the list
+     */
+    public void removeUserByIndexFromList(int i)
+    {
+        allUsers.remove(i);
+    }
+
+    public User getUserByUsername(String username)
+    {
+        for(User u: allUsers)
+        {
+            if(u.getUserName().equals(username))
+                return u;
+        }
+        return null;
+    }
+
+    /**
+     * check to see if the user already exists
+     * @param username the username to check
+     * @return true if the user exists, false otherwise
+     */
+    public boolean checkIfUserExists(String username)
+    {
+        for(User u: allUsers)
+        {
+            if(u.getUserName().equals(username))
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * check if user exists and set as currentUser
+     * @param uName the name of the user to check
+     * @return true if the user exists, false otherwise
+     */
+    public boolean checkAndSetUser(String uName)
+    {
+        if(checkIfUserExists(uName))
+        {
+            int spot = 0;
+            for(int i = 0; i < allUsers.size(); i++)
+            {
+                if(allUsers.get(i).getUserName().equals(uName))
+                {
+                    spot = i;
+                    break;
+                }
+            }
+            this.setCurrentUser(allUsers.get(spot));
+            this.userLoggedIn = true;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * saves the state of Admin instance to file
+     * @param pdA admin instance to save
+     * @throws IOException if an error occurs
+     */
+    public static void saveToFile(AdminUser pdA) throws IOException
+    {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(storeDirectory + File.separator + fileName)))
+        {
+            oos.writeObject(pdA);
+            oos.close();
+        }
+    }
+
+    /**
+     * Loads Admin data from file
+     * @return loaded admin instance
+     * @throws IOException if I/O error occurs
+     * @throws ClassNotFoundException if class not found
+     */
+    public static AdminUser loadFromFile() throws IOException, ClassNotFoundException
+    {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(storeDirectory + File.separator + fileName)))
+        {
+            return (AdminUser) ois.readObject();
+        }
+    }
+
 }
--- Photo68/src/photo68/model/Tag.java
+++ Photo68/src/photo68/model/Tag.java
@@ -1,12 +1,20 @@
+/*NOTES FOR OUTSELVES - TO BE REMOVED LATER
+ * its the code that you wrote, just moved it this package
+ * another copy still there in app package, still to see where it would be suited best
+ */
+
 package photo68.model;
 
+import java.io.Serializable;
+
 /**
- * Tag class
+ * This class represents a basic example class.
+ * It demonstrates how to implement Javadoc comments.
  * 
- * 
- * @author Oyku Pul, op116
+ * @author Oyku Pul
  */
-public class Tag {
+public class Tag implements Serializable
+{
     private String tagName;
     private String tagValue;
 
@@ -30,5 +38,22 @@ public class Tag {
 
     public void setTagValue(String tagValue) {
         this.tagValue = tagValue;
+    }
+
+    /**
+     * Method to compare tag values
+     * @param object to compare with
+     * @return true if tag values are equal, false otherwise
+     */
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(!(obj instanceof Tag) || obj == null)
+            return false;
+        Tag t = (Tag) obj;
+        boolean result1 = t.tagName.equals(this.tagName);
+        boolean result2 = t.tagValue.equals(this.tagValue);
+
+        return result1 && result2;
     }
 }
