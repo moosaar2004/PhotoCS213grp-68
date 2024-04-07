547307ca7c082cebd8158f442c21b252c0bf0667
moosaar2004 <moosaar2001@gmail.com>
2024-04-07 02:07:55 -0400

    ReupdatedModelFiled with AlbumClassAddition

--- Photo68/src/photo68/model/Tag.java
+++ Photo68/src/photo68/model/Tag.java
@@ -1,34 +1,60 @@
-package photo68.model;
-
-/**
- * Tag class
- * 
- * 
- * @author Oyku Pul, op116
+/*NOTES FOR OUTSELVES - TO BE REMOVED LATER
+ * its the code that you wrote, just moved it this package
+ * another copy still there in app package, still to see where it would be suited best
  */
-public class Tag {
-    private String tagName;
-    private String tagValue;
 
-    public Tag(String tagName, String tagValue) {
-        this.tagName = tagName;
-        this.tagValue = tagValue;
-    }
+ package photo68.model;
 
-    // Getters and setters
-    public String getTagName() {
-        return tagName;
-    }
-
-    public void setTagName(String tagName) {
-        this.tagName = tagName;
-    }
-
-    public String getTagValue() {
-        return tagValue;
-    }
-
-    public void setTagValue(String tagValue) {
-        this.tagValue = tagValue;
-    }
-}
+ import java.io.Serializable;
+ 
+ /**
+  * This class represents a basic example class.
+  * It demonstrates how to implement Javadoc comments.
+  * 
+  * @author Oyku Pul
+  */
+ public class Tag implements Serializable
+ {
+     private String tagName;
+     private String tagValue;
+ 
+     public Tag(String tagName, String tagValue) {
+         this.tagName = tagName;
+         this.tagValue = tagValue;
+     }
+ 
+     // Getters and setters
+     public String getTagName() {
+         return tagName;
+     }
+ 
+     public void setTagName(String tagName) {
+         this.tagName = tagName;
+     }
+ 
+     public String getTagValue() {
+         return tagValue;
+     }
+ 
+     public void setTagValue(String tagValue) {
+         this.tagValue = tagValue;
+     }
+ 
+     /**
+      * Method to compare tag values
+      * @param object to compare with
+      * @return true if tag values are equal, false otherwise
+      */
+     @Override
+     public boolean equals(Object obj)
+     {
+         if(!(obj instanceof Tag) || obj == null)
+             return false;
+         Tag t = (Tag) obj;
+         boolean result1 = t.tagName.equals(this.tagName);
+         boolean result2 = t.tagValue.equals(this.tagValue);
+ 
+         return result1 && result2;
+     }
+ }
+ 
